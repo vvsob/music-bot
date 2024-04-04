17b426be66491d73ad68e3e4cec4ea59ae0fd0e0
Oleg Sobolev <lerasimmus229@gmail.com>
2024-04-04 10:22:30 +0700

    split download

--- src/download.rs
+++ src/download.rs
@@ -8,14 +8,33 @@ pub struct DownloadError;
 pub fn download_from_youtube(url: &str) -> Result<Track, DownloadError> {
     let output = Command::new("yt-dlp")
         .args([
+            "--print",
+            "%(id)s %(duration)i",
+            "--no-playlist",
+            "--no-warnings",
+            "--",
+            url,
+        ]).output().unwrap();
+
+    let items: Vec<String> = std::str::from_utf8(&output.stdout).unwrap().split(' ').map(|s| s.replace("\n", "")).collect();
+
+    let filename = items[0].clone() + ".mp3";
+    let duration: u32 = items[1].parse().unwrap();
+
+    println!("{}", filename);
+    println!("{}", duration);
+
+    if duration > 900 {
+        return Err(DownloadError);
+    }
+
+    let output = Command::new("yt-dlp")
+        .args([
             "-o",
             "%(id)s",
             "--extract-audio",
             "--audio-format",
             "mp3",
-            "--print",
-            "%(id)s",
-            "--no-simulate",
             "--no-playlist",
             "--no-warnings",
             "--",
@@ -29,13 +48,6 @@ pub fn download_from_youtube(url: &str) -> Result<Track, DownloadError> {
         println!("{}", std::str::from_utf8(output.stdout.as_slice()).unwrap());
         return Err(DownloadError);
     }
-
-    let filename = std::str::from_utf8(output.stdout.as_slice())
-        .unwrap()
-        .replace('\n', "")
-        + ".mp3";
-
-    println!("{}", filename);
 
     let file_handle = FileHandle::new(Path::new(filename.as_str()));
     let info = TrackInfo::new(&filename);
